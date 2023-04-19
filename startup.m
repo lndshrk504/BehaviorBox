c1a11eda66f6d4d50efea618b3214943a2b16363
Will Snyder <william.snyder@swbell.net>
2023-04-19 10:21:28 -0400

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   startup.m
    Added startup.m, this folder will be on MATLAB path on training computers.

---
+++ startup.m
@@ -0,0 +1,9 @@
+desktop = fullfile(getenv('USERPROFILE'), 'Desktop');
+folderName = 'BehaviorBox\BB\';
+DesktopPath = fullfile(desktop, folderName);
+cd(DesktopPath);
+try
+    !git pull
+catch
+end 
+BehaviorBox_App
