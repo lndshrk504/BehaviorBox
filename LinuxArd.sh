67a88e3b337bfd99ddbc0d537ca3e1b9c14fc7ea
William Snyder <wsnyder@rockefeller.edu>
2023-10-05 14:22:30 -0400

    Quick fixes

--- LinuxArd.sh
+++ LinuxArd.sh
@@ -16,11 +16,11 @@ sudo apt update
 sudo apt install -y git
 
 # Define the repository location
-REPO_PATH="/home/$USER/your_mounted_directory/repo_name.git"
+REPO_PATH="/home/$USER/Dropbox\ \(Dropbox\ @RU\)/Git/bb/"
 
 # Clone the repository
 echo "Cloning repository from $REPO_PATH..."
-git clone "$REPO_PATH"
+git clone "$REPO_PATH" /home/$USER/Desktop/BehaviorBox
 
 # Display a message to inform the user to log out and back in
 echo "Installation complete. Please log out and log back in for group changes to take effect."
