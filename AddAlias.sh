b560734160bd972af51840855f08d00ec9b113cf
William Snyder <wsnyder@rockefeller.edu>
2023-10-05 15:36:30 -0400

    new file:   AddAlias.sh bash script to add aliases!

---
+++ AddAlias.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Function to add an alias if it doesn't exist
+add_alias() {
+    local alias_name="$1"
+    local alias_command="$2"
+
+    # Check if the alias already exists in .bashrc
+    if grep -q "alias ${alias_name}=" ~/.bash_aliases; then
+        echo "Alias ${alias_name} already exists."
+    else
+        # Add the alias to .bashrc
+        echo "alias ${alias_name}='${alias_command}'" >> ~/.bash_aliases
+        echo "Alias ${alias_name} added."
+    fi
+}
+
+# Examples of adding aliases
+#add_alias ll "ls -la"
+#add_alias md "mkdir -p"
+
+echo "Please restart your terminal or run 'source ~/.bashrc' to apply changes."
+
