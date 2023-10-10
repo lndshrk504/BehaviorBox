18ef0539f2cfae20e3cd2900cb42c84986db10c2
Will Snyder <wsnyder@rockefeller.edu>
2023-10-10 16:41:21 -0400

    Changes to be committed: 	modified:   viewDualCameras.m 	modified:   view_cameras_cuda.sh

--- viewDualCameras.m
+++ viewDualCameras.m
@@ -3,7 +3,7 @@ function viewDualCameras()
     sl = serialportlist("available");
     sl(contains(sl, '1')) = [];
     if numel(sl)>0
-       return
+       %return
     end
     % Step 2: Make sure you have the Image Acquisition Toolbox installed.
     output = ver;
@@ -11,7 +11,7 @@ function viewDualCameras()
         fprintf('Please install Image Acquisition Toolbox for dual camera view')
         return
     end
-    info = imaqhwinfo('winvideo');
+    info = imaqhwinfo('linuxvideo');
     CAMS = webcamlist;
     for IDs = cell2mat(info.DeviceIDs)
         if numel(CAMS) < IDs
@@ -20,7 +20,7 @@ function viewDualCameras()
         try
             deviceID1 = 1;  % Adjust the device ID if needed
             % Step 3: Initialize the video input objects for both cameras.
-            vid = videoinput('winvideo', IDs);
+            vid = videoinput('linuxvideo', IDs);
             % Step 4: Set the video resolution and format for both cameras. % WBS: This didn't work
             %vid1.VideoResolution = [640, 480];  % Adjust the resolution if needed
             %vid1.VideoFormat = 'YUY2_640x480';  % Adjust the format if needed
