9489569a6fe0f66ab010f3eb5a81f9ca627ff76e
will <will@neuro15b.local>
2023-11-03 15:56:26 -0400

    Changes to be committed: 	modified:   viewDualCameras.m

--- viewDualCameras.m
+++ viewDualCameras.m
@@ -6,15 +6,29 @@ function viewDualCameras()
         fprintf('Please install Image Acquisition Toolbox for dual camera view')
         return
     end
-    info = imaqhwinfo('linuxvideo');
-    CAMS = webcamlist;
-    if isempty(CAMS)
+% Make sure the cameras are free and have plenty of memory
+    imaqreset
+    imaqmex('feature','-limitPhysicalMemoryUsage',false)
+% Find cameras and remove system items
+    CamInfo = imaqhwinfo('linuxvideo');
+    INFO = CamInfo.DeviceInfo;
+    IDS = CamInfo.DeviceIDs;
+    List = struct2cell(INFO);
+    IDS = IDS(~cellfun(@(x) contains(x(end), '0x0'), List(end,:,:)));
+    if isempty(IDS)
         return
     end
-    for IDs = cell2mat(info.DeviceIDs)
+% Loop through the IDs and add cameras
+    for id = cell2mat(IDS)
+        info = INFO(id);
+        res = info.SupportedFormats; RES = res';
+        w = find(contains(res, 'YV12') & contains(res, '320x184'), 1, 'first');
+        if isempty(w)
+            w = find(contains(res, 'YV12') & contains(res, '320x'), 1, 'first');
+        end
         try
             %  Initialize the video input object
-            vid = videoinput('linuxvideo', IDs, 'YV12_320x184');
+            vid = videoinput('linuxvideo', id, res{w});
             %  Show a preview of the camera:
             preview(vid)
         catch err
