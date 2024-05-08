47cb2b13e37500c110c663f81e47f9821091e9f6
Will Snyder Neuro23F <wsnyder@rockefeller.edu>
2024-05-07 20:09:56 -0400

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	renamed:    Photogate.ino -> Photogate/Photogate.ino
    	renamed:    Rotary-Encoder-Arduino.ino -> Rotary-Encoder-Arduino/Rotary-Encoder-Arduino.ino

--- Photogate/Photogate.ino
+++ Photogate/Photogate.ino
@@ -1,22 +1,29 @@
 #define PIN_4 4
 #define PIN_5 5
 #define PIN_6 6
+#define PIN_7 7
 
 void setup() {
   pinMode(PIN_4, INPUT_PULLUP); // set pin 4 as input with internal pullup resistor
   pinMode(PIN_5, INPUT_PULLUP); // set pin 5 as input with internal pullup resistor
-  pinMode(PIN_6, OUTPUT); // set pin 6 as output
+  pinMode(PIN_6, INPUT_PULLUP); // set pin 6 as input with internal pullup resistor
+  pinMode(PIN_7, OUTPUT); // set pin 6 as output
   
   Serial.begin(9600); // start the serial at 9600 baud
 }
 
 void loop() {
   if (digitalRead(PIN_4) == LOW) {
-    Serial.println("Pin 4 is LOW");
+    Serial.print('L');
   }
-
-  if (digitalRead(PIN_5) == LOW) {
-    Serial.println("Pin 5 is LOW");
+  else if (digitalRead(PIN_5) == LOW) {
+    Serial.print('M');
+  }
+  else if (digitalRead(PIN_6) == LOW) {
+    Serial.print('R');
+  }
+  else{
+    Serial.print('-');
   }
 
   if (Serial.available() > 0) { // check if data is available to read on the serial port
@@ -26,5 +33,5 @@ void loop() {
     digitalWrite(PIN_6, LOW); // set pin 6 to LOW when there's no data being sent on the serial port
   }
 
-  delay(500); // delay for readability of the serial output, you can adjust this delay as needed
+  delay(10); // delay for readability of the serial output, you can adjust this delay as needed
 }
