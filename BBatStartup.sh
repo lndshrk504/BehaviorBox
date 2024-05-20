28345c6cb8b198ff79596fd7adc2a09705f13d65
will <wsnyder@rockefeller.edu>
2024-05-20 12:44:10 -0400

    Changes to be committed: 	modified:   BBatStartup.sh

--- BBatStartup.sh
+++ BBatStartup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-xfce4-terminal -e "bash -c 'export JAVA_TOOL_OPTIONS="-Djogl.disable.openglarbcontext=1"; matlab -nosplash -nodesktop -r "BehaviorBox_App"'"
+xfce4-terminal -e "bash -c 'export JAVA_TOOL_OPTIONS="-Djogl.disable.openglarbcontext=1"; matlab -nosplash -nodesktop -r "BehaviorBox_App"'; exec bash"
 
-xfce4-terminal -e "bash -c 'export JAVA_TOOL_OPTIONS="-Djogl.disable.openglarbcontext=1"; matlab -nosplash -nodesktop -r "viewDualCameras"'"
+xfce-terminal -e "bash -c 'export JAVA_TOOL_OPTIONS="-Djogl.disable.openglarbcontext=1"; matlab -nosplash -nodesktop -r "viewDualCameras"'; exec bash"
 
 sleep 30 
 
-xfce4-terminal -e "bash -c 'export JAVA_TOOL_OPTIONS="-Djogl.disable.openglarbcontext=1"; matlab -nosplash -nodesktop -r "BehaviorBox_App"'"
+xfce4-terminal -e "bash -c 'export JAVA_TOOL_OPTIONS="-Djogl.disable.openglarbcontext=1"; matlab -nosplash -nodesktop -r "BehaviorBox_App"'; exec bash"
 
