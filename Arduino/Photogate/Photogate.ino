08913be0992a700416adc5e1533234cd1d02927e
Will Snyder - Neuro23f <wsnyder@rockefeller.edu>
2024-10-22 13:05:03 -0400

    Changes to be committed: 	modified:   Arduino/Photogate/Photogate.ino Enable internal pull-up resistors on input pins.

--- Arduino/Photogate/Photogate.ino
+++ Arduino/Photogate/Photogate.ino
@@ -166,9 +166,9 @@ void handleStateChange() {
 
 void setup() {
   Serial.begin(115200);
-  pinMode(PIN_4, INPUT);
-  pinMode(PIN_5, INPUT);
-  pinMode(PIN_6, INPUT);
+  pinMode(PIN_4, INPUT_PULLUP);
+  pinMode(PIN_5, INPUT_PULLUP);
+  pinMode(PIN_6, INPUT_PULLUP);
   pinMode(PIN_7, OUTPUT);
   pinMode(PIN_8, OUTPUT);
   Serial.println();
