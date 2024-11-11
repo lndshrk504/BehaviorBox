f44ce09108fd3f91d840431258b9dc07e227b75a
Will Snyder - Neuro23f <wsnyder@rockefeller.edu>
2024-11-11 13:34:36 -0500

    Changes to be committed: 	modified:   DailyAnalysis.sh
    Log MATLAB output and timestamp in matlab_output.log
    
    Add timestamp to the log file
    Separate logs with a clear divider
    Append MATLAB output to the log file
    Direct error output to the log file

--- DailyAnalysis.sh
+++ DailyAnalysis.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-matlab -nodisplay -nosplash -r "run('GroupAnalysis.m'); exit;"
+# Write the current date and time to the log file
+echo "Log Date: $(date '+%Y-%m-%d %H:%M:%S')" >> matlab_output.log
+# Append a separator for clarity
+echo "-------------------------------------" >> matlab_output.log
+# Run the MATLAB script and append its output to the log file
+matlab -nodisplay -nosplash -r "run('GroupAnalysis.m'); exit;" > matlab_output.log 2>&1
