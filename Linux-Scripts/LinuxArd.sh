84308c986099c38fec7c562da6915d98c4db05b1
Will Snyder <willsnyder@Wills-MacBook-Pro.local>
2025-12-30 16:48:57 -0500

    Changes to be committed: 	modified:   Linux-Scripts/LinuxArd.sh Adding git config prefs to the script

--- Linux-Scripts/LinuxArd.sh
+++ Linux-Scripts/LinuxArd.sh
@@ -6,12 +6,22 @@ sudo apt update
 sudo ubuntu-drivers install 
 sudo apt install -y git vim-nox neofetch arduino v4l-utils ffmpeg
 sudo apt update && sudo apt autoremove -y
+
+# Configure global Git settings (will be the same on all machines)
+git config --global user.name "Will Snyder"
+git config --global user.email "wsnyder+${HOSTNAME}@rockefeller.edu"
+git config --global init.defaultBranch main
+git config --global core.editor "vim"
+# Example extra options you might want:
+# git config --global pull.rebase false
+git config --global color.ui auto
+
 # Add the current user to the dialout group to access the Arduino without root privileges
 sudo usermod -a -G dialout $USER
 
 # Define the repository location
-REPO_PATH="/home/$USER/Dropbox (Dropbox @RU)/Git/bb"
-CLONE_DIR="/home/$USER/Desktop/BehaviorBox"
+REPO_PATH="$HOME/Dropbox (Dropbox @RU)/Git/bb"
+CLONE_DIR="$HOME/Desktop/BehaviorBox"
 
 # Check if the intended clone directory exists and contains a .git subdirectory
 if [ -d "$CLONE_DIR/.git" ]; then
@@ -21,7 +31,7 @@ else
 fi
 
 cd $CLONE_DIR
-rm ~/.bashrc
+mv ~/.bashrc ~/.bashrc.backup
 ln -s $PWD/.bashrc ~/.bashrc
 
 # Display a message to inform the user to log out and back in
