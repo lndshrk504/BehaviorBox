a366309ad5f3777500bba322c188e5d2e867f848
Will Snyder <wsnyder+neuro26b@rockefeller.edu>
2026-04-15 15:58:05 -0400

    Removed DLC folder, migrated content to EyeTrack submodule

--- DLC/Tests/VerCheck.py
+++
@@ -1,4 +0,0 @@
-import deeplabcut, dlclive, tensorflow as tf
-print("DLC:", getattr(deeplabcut, "__version__", "n/a"))
-print("dlclive:", getattr(dlclive, "__version__", "n/a"))
-print("TF:", tf.__version__)
