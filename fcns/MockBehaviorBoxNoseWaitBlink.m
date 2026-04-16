65268b1c94ab89e135e7d900d58f30d71ccfd4a7
Will Snyder <willsnyder@Wills-MacBook-Pro.local>
2026-04-16 13:48:02 -0400

    Changes to be committed: 	modified:   fcns/MockBehaviorBoxNoseWaitBlink.m Adding idle blink to nose waiting period to start a new trial

--- fcns/MockBehaviorBoxNoseWaitBlink.m
+++ fcns/MockBehaviorBoxNoseWaitBlink.m
@@ -1,6 +1,7 @@
 classdef MockBehaviorBoxNoseWaitBlink < BehaviorBoxNose
     properties
         ReadyCueColorHistory double = zeros(0, 3)
+        StableChoiceCallCount double = 0
     end
 
     methods
@@ -11,6 +12,11 @@ classdef MockBehaviorBoxNoseWaitBlink < BehaviorBoxNose
         function setReadyCueColorSafe_(this, readyCueDot, color)
             this.ReadyCueColorHistory(end + 1, :) = double(color);
             setReadyCueColorSafe_@BehaviorBoxNose(this, readyCueDot, color);
+        end
+
+        function stable = Middle_StableChoice_StartTrial(this, checkDelay)
+            this.StableChoiceCallCount = this.StableChoiceCallCount + 1;
+            stable = Middle_StableChoice_StartTrial@BehaviorBoxNose(this, checkDelay);
         end
     end
 end
